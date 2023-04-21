9dd9b74c541c57b83b32c6a91fd46b1b969d9c15
arnau.armengol <arnau.armengol@estudiantat.upc.edu>
2023-04-21 11:10:10 +0200

    #T-94 i #T-178 funcionalitats per afegir una foto al usuari. Ara quan es crea l'usuari es pot pasar la imatge en base64 i aquesta es guardara al s3, al actualitzar l'usuari igual, si al actualitzar les dades del perfil, l'usuari canvia la foto, s'elimina l'antiga foto i s'emmagatzema la nova

--- src/main/java/backend/plugfinder/controllers/UserController.java
+++ src/main/java/backend/plugfinder/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
