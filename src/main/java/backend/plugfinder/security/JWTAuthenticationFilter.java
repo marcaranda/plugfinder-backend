f412bf2ff11b5b4dc998261b0ef77691f1f9b81b
arnau.armengol <arnau.armengol@estudiantat.upc.edu>
2023-05-10 09:07:37 +0200

    T-198: LOGIN trata ahora los usuarios eliminados, si un usuario esta eliminado no lo deja logearse, además se ha cambiado el código de error enviado y ahora envia un 401 en vez de un 403.

--- src/main/java/backend/plugfinder/security/JWTAuthenticationFilter.java
+++ src/main/java/backend/plugfinder/security/JWTAuthenticationFilter.java
@@ -1,20 +1,34 @@
 package backend.plugfinder.security;
 
+import backend.plugfinder.helpers.OurException;
+import backend.plugfinder.repositories.UserRepo;
+import backend.plugfinder.services.models.UserModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collections;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private UserRepo user_repository;
+
+    public JWTAuthenticationFilter(UserRepo user_repository) {
+        this.user_repository = user_repository;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         AuthCredentials credentials = new AuthCredentials();
@@ -23,13 +37,26 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         } catch (Exception e) {
         }
 
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                credentials.getEmail(),
-                credentials.getPassword(),
-                Collections.emptyList()
-        );
+        //Obtener el usuario de la base de datos a través del correo electrónico ingresado en el formulario de inicio de sesión
+        ModelMapper model_mapper = new ModelMapper();
+        UserModel us = model_mapper.map(user_repository.findOneByEmail(credentials.getEmail()), UserModel.class);
 
-        return getAuthenticationManager().authenticate(authenticationToken);
+        //Verificar si el usuario existe y no está eliminado
+        if (us != null && !us.isDeleted()) {
+            //Si el usuario no está eliminado, proceder a autenticarlo
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    credentials.getEmail(),
+                    credentials.getPassword(),
+                    Collections.emptyList()
+            );
+            return getAuthenticationManager().authenticate(authenticationToken);
+        }
+        else {
+            String errorMessage = "El usuario no existe";
+            OurException ourException = new OurException(errorMessage);
+
+            throw new BadCredentialsException(errorMessage, ourException);
+        }
     }
 
     @Override
--- src/main/java/backend/plugfinder/security/WebSecurityConfig.java
+++ src/main/java/backend/plugfinder/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package backend.plugfinder.security;
 
+import backend.plugfinder.helpers.OurException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import backend.plugfinder.repositories.UserRepo;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,11 +22,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @AllArgsConstructor
 public class WebSecurityConfig {
+    @Autowired
+    private UserRepo user_repository;
+
     private final UserDetailsService userDetailsService;
     private final JWTAuthorizationFilter jwtAuthorizationFilter; //Authorization filter
 
@@ -27,7 +39,7 @@ public class WebSecurityConfig {
     Define también que solicitudes web estan excluidas de los filtros y que dirección es la que activa la función*/
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
-        JWTAuthenticationFilter filter = new JWTAuthenticationFilter(); //Authentication filter
+        JWTAuthenticationFilter filter = new JWTAuthenticationFilter(user_repository); //Authentication filter
         filter.setAuthenticationManager(authManager);
         filter.setFilterProcessesUrl("/users/login");
 
@@ -36,23 +48,22 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/users/register").permitAll()
                 .requestMatchers("/chargers").permitAll()
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(filter)
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    // Manejo de la excepción y envío de respuesta de error al usuario
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+                })
+                .and()
                 .build();
-    }
 
-   /* @Bean
-    UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("admin").password(passwordEncoder().encode("admin")).roles().build());
-        return manager;
-    }*/
+    }
 
     @Bean
     AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
