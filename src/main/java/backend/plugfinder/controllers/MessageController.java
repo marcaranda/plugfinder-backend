70b67b21dc72b55379cd6fbfd149cf423f10d608
marcaranda <marc.aranda@estudiantat.upc.edu>
2023-06-06 22:24:11 +0200

    T-221: - inicio chat con sockets

---
+++ src/main/java/backend/plugfinder/controllers/MessageController.java
@@ -0,0 +1,24 @@
+package backend.plugfinder.controllers;
+
+import backend.plugfinder.controllers.dto.MessageDto;
+import backend.plugfinder.helpers.OurException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+    @Autowired
+    private SimpMessagingTemplate simp_messaging_template;
+    @Autowired
+    private ChatController chat_controller;
+
+    @MessageMapping("/message")
+    public MessageDto send_message(@Payload MessageDto message) throws OurException {
+        simp_messaging_template.convertAndSendToUser(String.valueOf(message.getTarget_id()), "/private", message);
+        chat_controller.save_message(message);
+        return message;
+    }
+}
